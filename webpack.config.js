8860b5d9f8390d1f874fd029794ae4dd76138c93
Fabrice Weinberg <Fabrice@weinberg.me>
2018-01-31 23:38:44 +0100

    simplify build

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,26 @@
 const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const { join } = require('path');
 
-const plugins = [];
-const entry = {
-  rclone: './src/index.js'
-};
 const config = env => {
-  if (env.production) {
-    plugins.push(
-      new (require('uglifyjs-webpack-plugin'))({
-        include: /\.min\.js$/
-      })
-    );
-    plugins.push(
-      new (require('webpack-bundle-analyzer')).BundleAnalyzerPlugin()
-    );
-
-    entry['rclone.min'] = entry.rclone;
-  }
-
   return {
-    entry,
+    entry: './src/index.js',
     output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: '[name].js'
+      path: join(__dirname, 'dist'),
+      libraryTarget: 'umd',
+      library: 'rclone'
     },
+    devtool: 'source-map',
     module: {
       rules: [
         {
           test: /\.js$/,
-          include: [path.resolve(__dirname, 'src')],
+          include: [join(__dirname, 'src')],
           use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['env']
-            }
+            loader: 'babel-loader'
           }
         }
       ]
-    },
-    plugins
+    }
   };
 };
 
