a92d9ee278d33b4be10ff51edfee8d0e398b583b
Alexander Lolis <alolis@rapiddot.com>
2020-04-15 12:45:14 +0300

    Implement support for empty salts
    If an empty string is used as a salt then it should be replaced with a default one as also done by rclone:
    https://github.com/rclone/rclone/blob/b9bd15a8c980c36f15dd9b90bc0c422e4020baec/backend/crypt/cipher.go#L175
    
    The default salt that must be used can be found here:
    https://github.com/rclone/rclone/blob/b9bd15a8c980c36f15dd9b90bc0c422e4020baec/backend/crypt/cipher.go#L54

--- src/constants.js
+++ src/constants.js
@@ -9,3 +9,4 @@ export const fileHeaderSize = fileMagicSize + fileNonceSize;
 export const blockHeaderSize = 16; // crypto_secretbox_BOXZEROBYTES
 export const blockDataSize = 64 * 1024;
 export const blockSize = blockHeaderSize + blockDataSize;
+export const defaultSalt = [0xa8, 0x0d, 0xf4, 0x3a, 0x8f, 0xbd, 0x03, 0x08, 0xa7, 0xca, 0xb8, 0x3e, 0x58, 0x1f, 0x86, 0xb1];
