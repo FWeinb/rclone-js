bfbf6746bf146e9931f95dc205831f8767b19577
Alexander Lolis <alolis@rapiddot.com>
2020-04-16 11:31:53 +0300

    Add test

--- src/rclone.test.js
+++ src/rclone.test.js
@@ -1,4 +1,6 @@
 import { Rclone } from './rclone';
+import {defaultSalt} from './constants';
+import PathCipher from './ciphers/PathCipher';
 
 test('both password and salt must be passed', done => {
   Rclone({
@@ -13,6 +15,25 @@ test('derive keys from password', done => {
   })
     .then(rclone => {
       expect(rclone).toMatchSnapshot('empty key rclone');
+      done();
+    })
+    .catch(err => done.fail(err));
+});
+
+test('use default salt when empty', done => {
+  // Generated encrypted string with the use of rclone and empty salt
+  const rcloneEncryptedFileName = 'ecrk8fu3e0pk86td3r634nan08';
+  const rcloneDecryptedFileName = 'encrypted_file'
+
+   Rclone({
+    password: 'UmyLSdRHfew6aual28-ggx78qHqSfQ',
+    salt: ''
+  })
+    .then(rclone => {
+      const pathCipher = PathCipher(rclone);
+      const decryptedString = pathCipher.decrypt(rcloneEncryptedFileName);
+
+      expect(decryptedString).toEqual(rcloneDecryptedFileName);
       done();
     })
     .catch(err => done.fail(err));
